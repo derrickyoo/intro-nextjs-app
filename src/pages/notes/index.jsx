fcb76d9316334d933f303e7978ba037d59cb9141
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2021-06-11 13:49:34 -0700

    example home button client-side navigation using router

--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const Page = () => (
-  <div>
-    <h1>Notes Index Page</h1>
-    <Link href="/notes/[params]" as={`/notes/1`}>
-      <a>
-        Note 1
-      </a>
-    </Link>
-  </div>
-)
+const Page = () => {
+  const router = useRouter()
+
+  return (
+    <div>
+      <h1>Notes Index Page</h1>
+  
+      <button onClick={e => router.push('/')}>
+        Home
+      </button>
+  
+      <Link href="/notes/[params]" as={`/notes/1`}>
+        <a>
+          Note 1
+        </a>
+      </Link>
+    </div>
+  )
+}
+
 
 export default Page

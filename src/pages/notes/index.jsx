1aaee6a4332b394ff70256aaaa2b658298fee171
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2021-06-11 13:35:13 -0700

    first Link added for client-side routing

--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Page = () => <div>Notes Index Page</div>
+const Page = () => (
+  <div>
+    <h1>Notes Index Page</h1>
+    <Link href="/notes/[params]" as={`/notes/1`}>
+      <a>
+        Note 1
+      </a>
+    </Link>
+  </div>
+)
 
 export default Page

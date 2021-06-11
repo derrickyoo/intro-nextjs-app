061dcf8afb9700f660fc61d558e29304effa60a0
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2021-06-11 13:12:25 -0700

    refactor /notes/:id to a catch-all route

--- pages/notes/[...params].jsx
+++ pages/notes/[...params].jsx
@@ -3,10 +3,10 @@ import { useRouter } from 'next/router'
 
 const Page = () => {
   const router = useRouter()
-  const { id } = router.query
+  const { params } = router.query
 
   return (
-    <div>Note { id }</div>
+    <div>Note</div>
   )
 }
 
